c0a8084738e844f4bfa493e5c92339c3efca8c26
unknown <alifarzamniadev@gmail.com>
2026-06-13 16:58:39 +0330

    Init

---
+++ build/build-debug/CMakeFiles/2dShooter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "2dShooter.lib"
+  "CMakeFiles/2dShooter.dir/src/main.cpp.obj"
+  "CMakeFiles/2dShooter.dir/src/main.cpp.obj.d"
+  "F:/c++/2dShooter/bin/Debug/2dShooter.exe"
+  "F:/c++/2dShooter/bin/Debug/2dShooter.exe.manifest"
+  "F:/c++/2dShooter/bin/Debug/2dShooter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2dShooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
